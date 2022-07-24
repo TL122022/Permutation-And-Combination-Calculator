890afe9ea7419b74a0886151e6488522ee3fbf1b
TL12 <101867993+TL122022@users.noreply.github.com>
2022-07-24 10:05:55 +0800

    Create permutation-calculator.py

---
+++ permutation-calculator.py
@@ -0,0 +1,17 @@
+import math
+
+def permutation(n, r):
+  factorial(n)/factorial(n - r)
+
+While True:
+  n = input("Enter the total number of objects/n: ")
+  r = input("Enter the number of objects selected/r: ")
+  q = input("Enter q or Q to exit")
+  
+  if q == 'q' or 'Q':
+    break
+  
+  else
+    permutation(n, r)
+  
+  
