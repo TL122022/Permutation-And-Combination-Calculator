cbd7b103543394afc1d2d97a6cead2c7a290e427
TL12 <101867993+TL122022@users.noreply.github.com>
2022-07-24 14:44:03 +0800

    Add Else Ifs

--- combination-calculator.py
+++ combination-calculator.py
@@ -11,4 +11,12 @@ def combination(n, r):
 n = input("Enter the total number of objects/n in the set: ")
 r = input("Enter the number of objects chosen/r in the set: ")
 
-combination(n, r)
+if n <= 0:
+    print("Input Error. Input n must be 1 or above")
+    break
+elif r > n:
+    print("Input Error. Input r must be less than or equal to n.")
+elif r <= 0:
+    print("Input Error. Input r must be greater than 0.")
+
+else combination(n, r)
