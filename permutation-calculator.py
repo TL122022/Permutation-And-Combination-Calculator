1a8b4484c9b8fb9b394aca06c8250f24b5994b79
TL12 <101867993+TL122022@users.noreply.github.com>
2022-07-24 14:42:57 +0800

    Update permutation-calculator.py

--- permutation-calculator.py
+++ permutation-calculator.py
@@ -11,4 +11,12 @@ def permutation(n, r):
 n = input("Enter the total number of objects/n: ")
 r = input("Enter the number of objects selected/r: ")
 
-permutation(n, r)
+if n <= 0:
+    print("Input Error. Input n must be 1 or above")
+    break
+elif r > n:
+    print("Input Error. Input r must be less than or equal to n.")
+elif r <= 0:
+    print("Input Error. Input r must be greater than 0.")
+
+else: permutation(n, r)
