0f85230d8934676f7377f6768416315eae5cf8f8
TL12 <101867993+TL122022@users.noreply.github.com>
2022-07-24 15:23:39 +0800

    Update combination-calculator.py

--- combination-calculator.py
+++ combination-calculator.py
@@ -13,7 +13,6 @@ r = input("Enter the number of objects chosen/r in the set: ")
 
 if n <= 0:
     print("Input Error. Input n must be 1 or above")
-    break
 elif r > n:
     print("Input Error. Input r must be less than or equal to n.")
 elif r <= 0:
